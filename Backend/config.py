e2386d1abeebbf14714ee06d30c5e2be5c0548a1
Harsh <harsh.s12346@gmail.com>
2026-06-19 14:06:04 +0530

    added neondb as database and added .env file

--- Backend/config.py
+++ Backend/config.py
@@ -1,12 +1,13 @@
 
 class Config:
     SECRET_KEY = 'SECRETEKEYFORAPP2'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///placement.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://neondb_owner:npg_ywaJNg2OMm6r@ep-twilight-frost-at8sjdok-pooler.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
     SECURITY_PASSWORD_SALT='SOMESALTTOTASTEAPP'
     SECURITY_PASSWORD_HASH = 'pbkdf2_sha512'
     SECURITY_TOKEN_AUTHENTICATION_HEADER = 'Authentication-Token'
     CACHE_TYPE = 'RedisCache'
-    CACHE_REDIS_URL = 'redis://localhost:6379/0'
+    REDIS_URL ='rediss://default:gQAAAAAAAVpaAAIgcDJhNTVkZDhmMzM3YmQ0N2E2YTkzMTgzOGFmMmRlN2ZjYg@heroic-peacock-88666.upstash.io:6379'
+    CACHE_REDIS_URL = REDIS_URL
     CACHE_DEFAULT_TIMEOUT=300
     MAIL_SERVER = 'localhost'
     MAIL_PORT = 1025
